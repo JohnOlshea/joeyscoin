eb57e279d761ff9de124f145e62d2a52f155f59a
John <johnolshea@gmail.com>
2024-03-03 21:28:36 +0100

    Working on porfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import Layout from './components/common/Layout'
 import { HomePage } from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PortfolioPage } from './components/PortfolioPage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path='/' element={<Layout><HomePage /></Layout>} />
+          <Route path='/portfolio' element={<Layout><PortfolioPage /></Layout>} />
         </Routes>
       </BrowserRouter>
     </>
