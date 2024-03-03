571f04425861f6817f54bf7ba3bba2c6b9865f49
John <johnolshea@gmail.com>
2024-03-03 21:31:46 +0100

    Add blog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Layout from './components/common/Layout'
 import { HomePage } from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BlogPages } from './components/BlogPages';
 import { PortfolioPage } from './components/PortfolioPage';
+import { ShopPage } from './components/ShopPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout><HomePage /></Layout>} />
           <Route path='/portfolio' element={<Layout><PortfolioPage /></Layout>} />
+          <Route path='/shop' element={<Layout><ShopPage /></Layout>} />
+          <Route path='/blog' element={<Layout><BlogPages /></Layout>} />
         </Routes>
       </BrowserRouter>
     </>
