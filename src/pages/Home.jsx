76c4c00769d12438d653bbc508adbf6e462d4608
John <johnolshea@gmail.com>
2024-03-04 09:59:14 +0100

    Cleanup and refactor

---
+++ src/pages/Home.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { BiBitcoin } from 'react-icons/bi'
+import { MdExplore } from 'react-icons/md'
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
+
+import { SliderContent } from '../components/SliderContent'
+import { TrendingAuthor } from '../components/TrendingAuthor'
+import { Banner } from '../components/Banner'
+
+import user1 from '../assets/images/user-1.jpg'
+import user2 from '../assets/images/user-2.jpg'
+import user3 from '../assets/images/user-3.jpg'
+import homeImg1 from '../assets/images/homeImg1.png'
+import homeImg2 from '../assets/images/homeImg2.png'
+import homeImg3 from '../assets/images/homeImg3.png'
+import { Collections } from '../components/Collections'
+
+export const HomePage = () => {
+  return (
+    <>
+      <Home />
+      <SliderContent />
+      <TrendingAuthor />
+      <HomeWrapper />
+      <Banner />
+      <Collections />
+    </>
+  )
+}
+
+export const Home = () => {
+  return (
+    <>
+      <section className='home'>
+        <div className="container">
+          <div className="left w-50">
+            <h1>
+              JoeysCoin <span>NFTs</span> <br />
+              Collection. Place
+              <br />
+              Your First Bid
+            </h1>
+            <button className='flex-b-g'>
+              Start using Coinroll today <HiOutlineArrowNarrowRight size={25} /> </button>
+          </div>
+          <div className="right">
+            <div className="card">
+              <div className="flex">
+                <div className="user-img">
+                  <img src={user1} alt="user" />
+                  <img src={user2} alt="" />
+                  <img src={user3} alt="" />
+                </div>
+                <label>1679+ users</label>
+              </div>
+              <br />
+              <h3>$5,020,680</h3>
+              <h5>+$1,400,100 +73.5%</h5>
+              <div className="flex">
+                <BiBitcoin size={25} />
+                <MdExplore size={25} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export const HomeWrapper = () => {
+  return (
+    <section className="home home-wrapper tagline">
+      <div className="container">
+        <div className="w-50">
+          <h1>
+            We work together to <br />
+            create beautiful products <br />
+            you will <span>love</span>.
+          </h1>
+        </div>
+        <div className="w-50 right">
+          <div className="images">
+            <img src={homeImg1} alt="" />
+            <img src={homeImg2} alt="" />
+            <img src={homeImg3} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
